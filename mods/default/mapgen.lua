22e12e4bf663d6e156adb9aaac3952ed43921973
Ezhh <Ezhh@users.noreply.github.com>
2022-08-23 18:02:07 +0100

    Adjust coral mapgen

--- mods/default/mapgen.lua
+++ mods/default/mapgen.lua
@@ -2409,34 +2409,33 @@ function default.register_decorations()
 
 	-- Coral reef
 
-	-- minetest.register_decoration({
-	-- 	name = "default:corals",
-	-- 	deco_type = "simple",
-	-- 	place_on = {"default:sand"},
-	-- 	place_offset_y = -1,
-	-- 	sidelen = 4,
-	-- 	noise_params = {
-	-- 		offset = -4,
-	-- 		scale = 4,
-	-- 		spread = {x = 50, y = 50, z = 50},
-	-- 		seed = 7013,
-	-- 		octaves = 3,
-	-- 		persist = 0.7,
-	-- 	},
-	-- 	biomes = {
-	-- 		"desert_ocean",
-	-- 		"savanna_ocean",
-	-- 		"rainforest_ocean",
-	-- 	},
-	-- 	y_max = -2,
-	-- 	y_min = -8,
-	-- 	flags = "force_placement",
-	-- 	decoration = {
-	-- 		"default:coral_green", "default:coral_pink",
-	-- 		"default:coral_cyan", "default:coral_brown",
-	-- 		"default:coral_orange", "default:coral_skeleton",
-	-- 	},
-	-- })
+	minetest.register_decoration({
+		name = "default:corals",
+		deco_type = "simple",
+		place_on = {"default:sand"},
+		place_offset_y = -1,
+		sidelen = 4,
+		noise_params = {
+			offset = -4,
+			scale = 4,
+			spread = {x = 50, y = 50, z = 50},
+			seed = 7013,
+			octaves = 3,
+			persist = 0.7,
+		},
+		biomes = {
+			"desert_ocean",
+			"savanna_ocean",
+			"rainforest_ocean",
+		},
+		y_max = -2,
+		y_min = -8,
+		flags = "force_placement",
+		decoration = {
+			"default:coral_green", "default:coral_pink",
+			"default:coral_cyan", 
+		},
+	})
 
 	-- Kelp
 
