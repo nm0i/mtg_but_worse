b35d25b83ca8e081e1fa9b0a986546f85a757526
nm0i <nm0i@me0w.net>
2022-08-25 02:07:05 +0300

    cfire + abrifire = love

---
+++ mods/cfire/init.lua
@@ -0,0 +1,23 @@
+local colors = {
+   "black", "blue", "cyan", "frosted", "green", "magenta", "orange", "purple", "red", "yellow",
+}
+
+for i = 1, #colors, 1 do
+   node_context = {
+      description = colors[i].." flame",
+      drawtype = "firelike",
+      tiles = {{
+            name=colors[i].."_fire_animated.png",
+            animation={
+               type="vertical_frames", aspect_w=16, aspect_h=16, length=1
+            },
+      }},
+      light_source = 12,
+      groups = {dig_immediate=3},
+      drop = '',
+      walkable = false,
+      buildable_to = true,
+      damage_per_second = 4,      
+   }
+   minetest.register_node("cfire:flame_"..colors[i], node_context)
+end
