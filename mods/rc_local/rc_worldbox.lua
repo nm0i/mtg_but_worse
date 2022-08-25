4de0ffeba3630d8f90352e52e848903ade41e8fc
nm0i <nm0i@me0w.net>
2022-08-26 02:53:26 +0300

    refactoring worldbox limits

--- mods/rc_local/rc_worldbox.lua
+++ mods/rc_local/rc_worldbox.lua
@@ -8,28 +8,28 @@ local sign = function(x)
    end
 end
 
-local config_setting  = "alt_spawnpoint"
-local spawnpoint = minetest.setting_get_pos(config_setting)
 
-local worldbox_limit = 2048
-minetest.register_globalstep(function(dtime)
-      for _, player in pairs(minetest.get_connected_players()) do
-         local name = player:get_player_name()
-         local pos = player:get_pos()
-         if math.abs(pos.x) > worldbox_limit then
-            pos.x = sign(pos.x)*(worldbox_limit - 4)
-            player:moveto(pos,false)
-            minetest.chat_send_player(name,"Invisible force pulls you back")
-         elseif math.abs(pos.z) > worldbox_limit then
-            pos.z = sign(pos.z)*(worldbox_limit - 4)
-            player:moveto(pos,false)
-            minetest.chat_send_player(name,"Invisible force pulls you back")
-         elseif pos.y < -1024 then
-            pos.z =0
-            pos.y=2100
-            pos.x=0
+local worldbox_flat_limit = 2048
+local worldbox_vertical_limit = 1024
+local func_context = function(dtime)
+   for _, player in pairs(minetest.get_connected_players()) do
+      local name = player:get_player_name()
+      local pos = player:get_pos()
+      if math.abs(pos.x) > worldbox_flat_limit then
+         pos.x = sign(pos.x)*(worldbox_flat_limit - 4)
+         player:moveto(pos,false)
+         minetest.chat_send_player(name,"Invisible force pulls you back")
+      elseif math.abs(pos.z) > worldbox_flat_limit then
+         pos.z = sign(pos.z)*(worldbox_flat_limit - 4)
+         player:moveto(pos,false)
+         minetest.chat_send_player(name,"Invisible force pulls you back")
+      elseif math.abs(pos.y) > worldbox_vertical_limit then
+         local spawnpoint = minetest.setting_get_pos("static_spawnpoint")
+         if spawnpoint then
             player:setpos(spawnpoint,false)
             minetest.chat_send_player(name,"You went too deep and died...")
          end
       end
-end)
+   end
+end
+minetest.register_globalstep(func_context)
